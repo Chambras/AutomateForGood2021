7be3f50f69ce3e3b16b2dcc9d8fafc8db05ee682
Marcelo Zambrana Villarroel <marcech.am@gmail.com>
2021-10-22 00:49:06 -0400

    initial commit

---
+++ Chef/cookbooks/KafkaServer/attributes/default.rb
@@ -0,0 +1,12 @@
+default['kafkaServer']['kafkaRepo'] = 'https://archive.apache.org/dist/kafka/'
+default['kafkaServer']['kafkaVersion'] = '2.3.0'
+
+# TFMS configuration
+default['kafkaServer']['SWIMEndpointPort'] = 55443
+default['kafkaServer']['SWIMVPN'] = 'TFMS'
+
+## TFMS Secrets moved to a data bag
+# default['kafkaServer']['SWIMEndpoint']=''
+# default['kafkaServer']['SWIMUserNaMe']=''
+# default['kafkaServer']['Password']=''
+# default['kafkaServer']['SWIMQueue']=''
